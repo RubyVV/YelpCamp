265b88412b5aa6517444e4f620dcaf59c9bf1dd2
RubyVV <rubyzhao09@gmail.com>
2022-08-07 22:47:52 -0700

    ready to try deploying

---
+++ seeds/index.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 300; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            //YOUR USER ID
+            author: '62ec85bf2438cd630e1fcee5',
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum animi eum, saepe ipsum culpa ea sequi sint exercitationem autem molestiae inventore mollitia optio illum quis sunt tempora molestias. Qui, sint?',
+            price,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
+            },
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dkmk0qsy3/image/upload/v1659811111/YelpCamp/lm4omkydiqp1cufmuyml.jpg',
+                    filename: 'YelpCamp/hlbwkk5y10q1m3qubwfx'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dkmk0qsy3/image/upload/v1659811111/YelpCamp/hlbwkk5y10q1m3qubwfx.jpg',
+                    filename: 'YelpCamp/lm4omkydiqp1cufmuyml'
+                }
+
+            ]
+
+        })
+        await camp.save();
+    };
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+});
