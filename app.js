10d4de45af787768bdf3db1adecd5e5e3a686fb2
RubyVV <rubyzhao09@gmail.com>
2022-08-07 22:59:24 -0700

    add start script

--- app.js
+++ app.js
@@ -157,6 +157,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3500, () => {
-    console.log(`Serving on port 3500`);
-});
+const port = process.env.PORT || 3500;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
+})
